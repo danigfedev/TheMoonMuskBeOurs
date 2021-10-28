ee368a5d4e31784dad14e85250c951830827519b
danigfedev <danigfe.dev@gmail.com>
2021-10-28 11:11:52 +0200

    [Optimization] Added simple FPS debug UI

--- U_TheMoonMuskBeOurs/Assets/0Assets/Scripts/Optimization/FrameRateController.cs
+++ U_TheMoonMuskBeOurs/Assets/0Assets/Scripts/Optimization/FrameRateController.cs
@@ -4,20 +4,34 @@ using UnityEngine;
 
 public class FrameRateController : MonoBehaviour
 {
+    [Header("FPS Control settings")]
     [SerializeField] bool debugFPS = false;
     [SerializeField] int debugStep = 15;
 
+    [Space(5)]
+    [Header("Debugging")]
+    [SerializeField] GameObject canvas;
+    [SerializeField] TMPro.TextMeshProUGUI fpsLabel;
+
+
     private void Awake() => SetApplicationTargetFrameRate(60);
+
+    private void Start() => canvas.SetActive(false);
+
 
     public void Update()
     {
         if (debugFPS)
         {
+            if(!canvas.activeInHierarchy) canvas.SetActive(true);
             if (Time.frameCount % debugStep == 0)
             {
-                Debug.Log(string.Format("FPS: {0}", 1 / Time.deltaTime));
+                fpsLabel.text = string.Format("FPS: {0}", 1 / Time.deltaTime);
+                //Debug.Log(string.Format("FPS: {0}", 1 / Time.deltaTime));
             }
         }
+        else
+            if (canvas.activeInHierarchy) canvas.SetActive(false);
     }
 
     public void SetApplicationTargetFrameRate(int targetFR) => Application.targetFrameRate = targetFR;
