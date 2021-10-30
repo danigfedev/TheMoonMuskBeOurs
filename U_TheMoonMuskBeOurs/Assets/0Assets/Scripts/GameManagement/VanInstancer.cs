89b9dc468fe507211b7a514b1f5c98362b7de5ad
danigfedev <danigfe.dev@gmail.com>
2021-10-31 01:59:06 +0200

    [Gameplay] Object Instancer scripts refactoring

--- U_TheMoonMuskBeOurs/Assets/0Assets/Scripts/GameManagement/VanInstancer.cs
+++ U_TheMoonMuskBeOurs/Assets/0Assets/Scripts/GameManagement/VanInstancer.cs
@@ -3,26 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(ObjectPooler))]
-public class VanInstancer_Test : MonoBehaviour
+public class VanInstancer : MonoBehaviour
 {
-    private struct ScreenExtentsWorldSpace
-    {
-        public float xMin, xMax, yMin, yMax, xMid, yMid, width, height;
-    }
-
-
-    [SerializeField] int screenHeight;
-    [SerializeField] int screenWidth;
     [Range(0, 1)]
     [SerializeField] float verticalPercentageFromBottom = 0.75f;
     [SerializeField] float verticalOffsetLinearMotion = 2.5f;
 
-    [Header("== TESTING ==")]
-    [SerializeField] Vector3 rotationPivot; //For testing purposes
-    [SerializeField] Vector3 spawnLateralPosition; //For testing purposes
-    //[SerializeField] int screenHeight;
-    //[SerializeField] int screenWidth;
-
+    Vector3 rotationPivot;
     ObjectPooler objectPooler;
 
     void Awake()
@@ -45,12 +32,8 @@ public class VanInstancer_Test : MonoBehaviour
 
         float Xoffset =  3 * extents.x;
         float Yoffset =  3 * extents.y;
-        //float horDisplacement = 2 * extents.x + horoffset;
 
-        //float verticalInitialPosition = topScreen.y + verticalOffset;
-        //float vertDisplacement = verticalInitialPosition + 2 * extents.y;
-
-        ScreenExtentsWorldSpace worldLimits = GetScreenWorldExtents();
+        ScreenExtentsWorldSpace worldLimits = Utils.GetScreenWorldExtents();
 
         //Calculate pivot point:
         float pivotY = worldLimits.yMin + worldLimits.height * verticalPercentageFromBottom;
@@ -60,7 +43,6 @@ public class VanInstancer_Test : MonoBehaviour
         float verticalvalue = pivotY - verticalOffsetLinearMotion;
 
         float horizontalValue = worldLimits.xMax + Xoffset;
-        //float horizontalValueNegative = -worldLimits.xMax - Xoffset;
 
         //Screen Horizontal limit + offset
 
@@ -90,60 +72,6 @@ public class VanInstancer_Test : MonoBehaviour
         endPosition = new Vector3(rotationPivot.x + Xoffset, rotationPivot.y - Xoffset);
         objPack[3].GetComponent<Enemy_Van_Controller>().SetMotionParameters(objPack[3].transform.position, endPosition, rotationPivot);
 
-
-
-        /*
-        objPack[0].transform.position = new Vector3(rotationPivot.x - offset, rotationPivot.y - offset);
-        objPack[1].transform.position = new Vector3(rotationPivot.x - offset, rotationPivot.y + offset);
-        objPack[2].transform.position = new Vector3(rotationPivot.x + offset, rotationPivot.y - offset);
-        objPack[3].transform.position = new Vector3(rotationPivot.x + offset, rotationPivot.y + offset);
-
-        foreach (GameObject enemy in objPack)
-        {
-            //enemy.GetComponent<Enemy_Van_Controller>().SetPivotPoint(spawnPosition);
-            enemy.GetComponent<Enemy_Van_Controller>().StartMoving(,rotationPivot);
-        }
-        */
     }
 
-    /// <summary>
-    /// Returns screen's extents in world coordinates
-    /// </summary>
-    /// <returns></returns>
-    private ScreenExtentsWorldSpace GetScreenWorldExtents()
-    {
-        Vector3 screenDimensions;
-#if UNITY_ANDROID && !UNITY_EDITOR
-        screenDimensions = new Vector3(Screen.width, Screen.height, 0);
-#else
-        screenDimensions = new Vector3(screenWidth, screenHeight, 0);
-#endif
-
-        Vector3 posMin = Camera.main.ScreenToWorldPoint(Vector3.zero);
-        Vector3 posMax = Camera.main.ScreenToWorldPoint(screenDimensions);
-
-        ScreenExtentsWorldSpace extentsWorld;
-        extentsWorld.xMin = posMin.x;
-        extentsWorld.xMax = posMax.x;
-        extentsWorld.yMin = posMin.y;
-        extentsWorld.yMax = posMax.y;
-
-        float width = extentsWorld.xMax - extentsWorld.xMin;
-        float height = extentsWorld.yMax - extentsWorld.yMin;
-        extentsWorld.width = width;
-        extentsWorld.height = height;
-
-        extentsWorld.xMid = posMin.x + width / 2;
-        extentsWorld.yMid = posMin.y + height / 2;
-
-        Debug.Log(string.Format("xMin: {0} | xMax: {1} | yMin: {2} | yMax: {3}",
-            extentsWorld.xMin,
-            extentsWorld.xMax,
-            extentsWorld.yMin,
-            extentsWorld.yMax));
-
-        return extentsWorld;
-
-        //return pos;
-    }
 }
